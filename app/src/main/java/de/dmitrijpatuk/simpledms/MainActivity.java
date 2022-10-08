27c193c6804cccbdbf60e7f05d28689235ef9b98
DmitrijP <dmitrij.patuk@gmx.de>
2022-10-08 14:10:46 +0200

    initial commit

---
+++ app/src/main/java/de/dmitrijpatuk/simpledms/MainActivity.java
@@ -0,0 +1,26 @@
+package de.dmitrijpatuk.simpledms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.dmitrijpatuk.simpledms.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.hello_button);
+        TextView textView1 = findViewById(R.id.hello_textview);
+        button.setOnClickListener(l -> {
+            textView1.setText(R.string.hello_world_textview_text);
+        });
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# 2. Android Basics: FindViewById VS DataBinding VS ViewBinding
+
+## Ziel
+Wie manipuliert man das UI einer Android App? 
+Selektion von UI Elementen.
+Click Events.
+
+
+## Vorkenntnisse
+- Java Basics
+- Objektorientierte Entwicklung
+- Android Studio IDE
+
+
+## Links
+- GitHub: https://github.com/DmitrijP
+- ViewBinding: https://developer.android.com/topic/libraries/view-binding
+- DataBinding: https://developer.android.com/topic/libraries/data-binding/#java
